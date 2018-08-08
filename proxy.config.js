931de21e1b6dd0694c1d9426dc2dd1bcf9188d0f
Mr.zhang <540784419@qq.com>
2018-08-08 16:26:15 +0800

    反向代理配置

---
+++ proxy.config.js
@@ -0,0 +1,7 @@
+const localUrl = 'http://10.2.56.34:8103/tdpBackGround'
+
+module.exports = {
+  '/Story/getProject': localUrl,
+  '/Story/getVersion': localUrl,
+  '/Story/getAllStory': localUrl,
+}
