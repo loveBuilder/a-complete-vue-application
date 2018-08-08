2752f4023e644a6790fedb7efd9c7bddac7f1018
Mr.zhang <540784419@qq.com>
2018-08-08 16:44:49 +0800

    故事墙-index

---
+++ src/components/storyWall/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="root">
+    <Header />
+    <Center />
+  </div>
+</template>
+
+<script>
+import Header from './header'
+import Center from './center'
+export default {
+  name: 'StoryWall',
+  components: {
+    Header,
+    Center
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import './story.scss';
+</style>
